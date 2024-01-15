d90c048a0a749baee29b07ace6ac3e4850d3af58
Yadel Galindo <yadelgalindolido@alumno.ieselrincon.es>
2024-01-15 09:13:53 +0000

    Archivo subido por Yadel

---
+++ validar.php
@@ -0,0 +1,13 @@
+<?php
+// Validar los datos recibidos
+$nombre = $_GET['nombre'];
+$apellidos = $_GET['apellidos'];
+$email = $_GET['email'];
+$contraseña = $_GET['contraseña'];
+$Asignaturas = $_GET['Asignatura'];
+$Deporte = $_GET['Deporte'];
+$ciudades = $_GET['ciudad'];
+echo ($nombre);
+echo ($apellidos);
+echo ($Asignaturas);
+echo ($ciudades);
